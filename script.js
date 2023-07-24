3faabc94e0ca7e33d136fcdb49957a5feb5ce1c1
AlmadelX <almadelx@proton.me>
2023-07-24 18:12:57 +0500

    Add move logic

--- script.js
+++ script.js
@@ -1,37 +1,76 @@
 // Modules
+
 const game = (() => {
+  // Factories
   function playerFactory(name, marker) {
     return { name, marker };
   }
 
+  // Properties
   let players;
-  let currentPlayer = 0;
-  let board = [];
+  let currentPlayer;
+  let playing = false;
+  let board;
+
+  // Methods
 
   function start(xPlayerName, oPlayerName) {
+    // Initialize properties
     players = [
       playerFactory(xPlayerName, 'X'),
       playerFactory(oPlayerName, 'O'),
     ];
+    currentPlayer = 0;
+    playing = true;
+    board = new Array(9);
   }
 
-  function getCurrentPlayer() {
+  // If the move is possible, performs it and returns true, otherwise immediately returns false
+  function move(id) {
+    if (!playing || board[id]) {
+      return false;
+    }
+
+    board[id] = players[currentPlayer].marker;
+    currentPlayer = (currentPlayer + 1) % 2;
+    return true;
+  }
+
+  // Getters
+
+  function getCurrentPlayerName() {
     return players[currentPlayer].name;
   }
 
-  return { start, getCurrentPlayer };
+  function getBoard() {
+    return board;
+  }
+
+  return { start, move, getCurrentPlayerName, getBoard };
 })();
 
 const displayController = (() => {
+  // Dom elements
   const form = document.querySelector('form');
   const moveInfo = document.querySelector('.move-info');
+  const cells = document.querySelectorAll('.cell');
 
-  function getForm() {
-    return form;
-  }
+  // Methods
 
   function toggleElement(element) {
     element.classList.toggle('hidden');
+  }
+
+  function updateMoveInfo() {
+    const name = moveInfo.querySelector('.marker');
+    name.textContent = game.getCurrentPlayerName();
+  }
+
+  function updateBoard() {
+    const board = game.getBoard();
+    for (let i = 0; i < 9; ++i) {
+      cells[i].textContent = board[i];
+    }
   }
 
   function handleForm() {
@@ -50,22 +89,28 @@ const displayController = (() => {
     updateMoveInfo();
   }
 
-  function updateMoveInfo() {
-    const player = moveInfo.querySelector('.marker');
-    player.textContent = game.getCurrentPlayer();
+  function handleClick(id) {
+    if (game.move(id)) {
+      updateMoveInfo();
+      updateBoard();
+    }
   }
 
-  return { getForm, handleForm };
+  function setupPage() {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      handleForm();
+    });
+
+    cells.forEach((cell, id) => {
+      cell.addEventListener('click', e => {
+        handleClick(id);
+      });
+    });
+  }
+
+  return { setupPage };
 })();
 
 // Page setup
-function setup() {
-  const form = displayController.getForm();
-
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-    displayController.handleForm();
-  });
-}
-
-setup();
+displayController.setupPage();
